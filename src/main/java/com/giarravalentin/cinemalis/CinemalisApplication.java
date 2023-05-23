2580936ae0b7a6f62360bc00e1fb4b5ad08fd242
Valentin <valentingiarra2@gmail.com>
2023-05-23 09:26:34 -0300

    inicio

---
+++ src/main/java/com/giarravalentin/cinemalis/CinemalisApplication.java
@@ -0,0 +1,13 @@
+package com.giarravalentin.cinemalis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CinemalisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CinemalisApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/giarravalentin/cinemalis/CinemalisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.giarravalentin.cinemalis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CinemalisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
